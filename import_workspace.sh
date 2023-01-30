0f4c50fdb39e61678125c23380adccbb49a50e85
v4hn <me@v4hn.de>
2023-01-30 10:25:37 +0100

    support checking out individual stages only

--- import_workspace.sh
+++ import_workspace.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 mkdir ws 2>/dev/null && echo "creating new folder 'ws'" || echo "using existing folder 'ws'"
 
-for f in *\.repos; do
+if [[ -n "$1" ]]; then
+  REPOS="$@"
+else
+  eval REPOS=*\.repos
+fi
+
+for f in $REPOS; do
   echo importing $f
   vcs import ws <$f >/dev/null
 done
