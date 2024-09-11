4ff59a67a8de04d99dfef52c447b4b7cda3c2659
v4hn <me@v4hn.de>
2024-09-11 22:48:25 +0200

    add a docker helper that binds the repository

---
+++ docker/run_latest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# current folder
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PDIR="$(dirname "$DIR")"
+
+exec docker run -it --mount type=bind,source=$PDIR,destination=/ros-o-builder ros-one-jammy:latest bash -l
