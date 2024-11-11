6fa3c8ba1e7e391c57beb47f9d1aef5807c78590
v4hn <me@v4hn.de>
2024-11-11 15:06:51 +0100

    new helper to list all failed packages quickly

---
+++ scripts/show-failed-packages.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+distro=$1
+if [ -z "$distro" ]; then
+   distro=bookworm
+fi
+
+REPO=https://raw.githubusercontent.com/v4hn/ros-o-builder/refs/heads/$distro-one-unstable
+
+# csv header is
+# Package,Version,URL,Status,Bloom Log,Build Log,Deb File,Installed Files
+
+curl -s $REPO/pkg_build_status.csv | tail -n+2 | awk -F, -v repo=$REPO/repository '
+$4 != "success" {
+   if($4 == "failed-bloom-generate"){
+      report=$5;
+   } else {
+      report=$6;
+   }
+   print("Package: " $1 "\nLog: " repo "/" report "\nUpstream: " $3 "\n---")
+}
+'
